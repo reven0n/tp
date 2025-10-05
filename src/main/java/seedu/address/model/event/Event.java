42fc7551b841937b6400421e50bcf082b9b67cbf
asaiyume <57011411+asaiyume@users.noreply.github.com>
2025-10-05 17:25:13 +0800

    Fix checkstyle for CICD

--- src/main/java/seedu/address/model/event/Event.java
+++ src/main/java/seedu/address/model/event/Event.java
@@ -1,10 +1,11 @@
 package seedu.address.model.event;
 
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.person.Person;
 
