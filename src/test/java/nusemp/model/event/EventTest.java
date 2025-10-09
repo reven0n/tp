419543d834a2798d1e6948fd4a135685ff4e6ac3
rizrn <89007417+rizrn@users.noreply.github.com>
2025-10-09 22:32:08 +0800

    Add Event test

--- src/test/java/nusemp/model/event/EventTest.java
+++ src/test/java/nusemp/model/event/EventTest.java
@@ -1,6 +1,9 @@
 package nusemp.model.event;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -71,9 +74,8 @@ class EventTest {
         EventName name = new EventName("Meeting");
         EventDate date = new EventDate("01-10-2025 14:00");
 
-        Set<Person> participants1 = new HashSet<Person>();
+        Set<Person> participants1 = createParticipantSet("Bob");
         Person bob = new PersonBuilder().withName("Bob").build();
-        participants1.add(bob);
         Set<Person> participants2 = new HashSet<Person>();
 
         Event event1 = new Event(name, date, participants1);
@@ -89,7 +91,7 @@ class EventTest {
 
         Event event1 = new Event(name, date1);
         Event event2 = new Event(name, date2);
-        assertEquals(true, event1.isSameEvent(event2));
+        assertTrue(event1.isSameEvent(event2));
     }
 
     @Test
@@ -100,7 +102,7 @@ class EventTest {
 
         Event event1 = new Event(name1, date);
         Event event2 = new Event(name2, date);
-        assertEquals(false, event1.isSameEvent(event2));
+        assertFalse(event1.isSameEvent(event2));
     }
 
     @Test
@@ -112,7 +114,7 @@ class EventTest {
 
         Event event1 = new Event(name1, date1);
         Event event2 = new Event(name2, date2);
-        assertEquals(false, event1.isSameEvent(event2));
+        assertFalse(event1.isSameEvent(event2));
     }
 
     @Test
@@ -122,23 +124,86 @@ class EventTest {
 
         Event event1 = new Event(name, date);
         Event event2 = new Event(name, date);
-        assertEquals(true, event1.isSameEvent(event2));
+        assertTrue(event1.isSameEvent(event2));
     }
 
     @Test
     public void equals_sameValues_returnsTrue() {
+        EventName name1 = new EventName("Meeting");
+        EventDate date1 = new EventDate("01-10-2025 14:00");
+        Set<Person> participants1 = createParticipantSet("Alice", "Bob");
+
+        EventName name2 = new EventName("Meeting");
+        EventDate date2 = new EventDate("01-10-2025 14:00");
+        Set<Person> participants2 = createParticipantSet("Alice", "Bob");
+
+        Event event1 = new Event(name1, date1, participants1);
+        Event event2 = new Event(name2, date2, participants2);
+        assertEquals(event1, event2);
+    }
+
+    @Test
+    public void equals_sameObject_returnsTrue() {
         EventName name = new EventName("Meeting");
         EventDate date = new EventDate("01-10-2025 14:00");
+        Event event = new Event(name, date);
+        assertEquals(event, event);
+    }
 
-        Set<Person> participants = new HashSet<Person>();
-        Person alice = new PersonBuilder().withName("Alice").build();
-        Person bob = new PersonBuilder().withName("Bob").build();
+    @Test
+    public void equals_null_returnsFalse() {
+        EventName name = new EventName("Meeting");
+        EventDate date = new EventDate("01-10-2025 14:00");
+        Event event = new Event(name, date);
+        assertNotEquals(null, event);
+    }
 
-        participants.add(alice);
-        participants.add(bob);
+    @Test
+    public void equals_differentParticipants_returnsFalse() {
+        EventName name1 = new EventName("Meeting");
+        EventDate date1 = new EventDate("01-10-2025 14:00");
+        Set<Person> participants1 = createParticipantSet("Alice", "Bob");
 
-        Event event1 = new Event(name, date, participants);
-        Event event2 = new Event(name, date, participants);
-        assertEquals(true, event1.equals(event2));
+        EventName name2 = new EventName("Meeting");
+        EventDate date2 = new EventDate("01-10-2025 14:00");
+        Set<Person> participants2 = createParticipantSet("Alice", "Charlie");
+
+        Event event1 = new Event(name1, date1, participants1);
+        Event event2 = new Event(name2, date2, participants2);
+        assertNotEquals(event1, event2);
+    }
+
+    @Test
+    public void equals_differentName_returnsFalse() {
+        EventName name1 = new EventName("Meeting");
+        EventName name2 = new EventName("Conference");
+        EventDate date = new EventDate("01-10-2025 14:00");
+
+        Set<Person> participants = createParticipantSet("Alice", "Bob");
+
+        Event event1 = new Event(name1, date, participants);
+        Event event2 = new Event(name2, date, participants);
+        assertNotEquals(event1, event2);
+    }
+
+    @Test
+    public void equals_differentDate_returnsFalse() {
+        EventName name = new EventName("Meeting");
+        EventDate date1 = new EventDate("01-10-2025 14:00");
+        EventDate date2 = new EventDate("02-10-2025 14:00");
+
+        Set<Person> participants = createParticipantSet("Alice", "Bob");
+
+        Event event1 = new Event(name, date1, participants);
+        Event event2 = new Event(name, date2, participants);
+        assertNotEquals(event1, event2);
+    }
+
+    private Set<Person> createParticipantSet(String... names) {
+        Set<Person> participants = new HashSet<>();
+        for (String name : names) {
+            participants.add(new PersonBuilder().withName(name).build());
+        }
+        return participants;
     }
 }
