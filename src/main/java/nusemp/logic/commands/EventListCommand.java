8cac91c1cf9d530471e9290ec46ef8ab1d1e01c5
reven0n <darientan.ssis@gmail.com>
2025-10-10 23:55:38 +0800

    Add EventListCommand Class
    Add and implement the EventListCommand Class to
    support the listing of events

---
+++ src/main/java/nusemp/logic/commands/EventListCommand.java
@@ -0,0 +1,30 @@
+package nusemp.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+import static nusemp.model.Model.PREDICATE_SHOW_ALL_EVENTS;
+
+import nusemp.logic.Messages;
+import nusemp.model.Model;
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class EventListCommand extends Command {
+
+    public static final String COMMAND_WORD = "list";
+
+    // public static final String MESSAGE_SUCCESS = "Listed all events"; // archive
+
+    public static final String MESSAGE_USAGE = CommandType.EVENT + " " + COMMAND_WORD
+            + ": Lists all events in the event book.\n"
+            + "Example: " + CommandType.EVENT + " " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
+        return new CommandResult(String.format(Messages.MESSAGE_EVENTS_LISTED_OVERVIEW,
+                model.getFilteredEventList().size()));
+    }
+}
