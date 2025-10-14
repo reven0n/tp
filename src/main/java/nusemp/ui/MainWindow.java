d4fdb4789585aaddad24e9e201ba3a693bb399fd
ywijie <yijieew@gmail.com>
2025-10-14 16:10:10 +0800

    Cleaned up commented code to merge

--- src/main/java/nusemp/ui/MainWindow.java
+++ src/main/java/nusemp/ui/MainWindow.java
@@ -143,13 +143,11 @@ public class MainWindow extends UiPart<Stage> {
         personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
 
         resultDisplay = new ResultDisplay();
-        //resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
 
         StatusBarFooter statusBarFooter = new StatusBarFooter(logic.getAddressBookFilePath());
         statusbarPlaceholder.getChildren().add(statusBarFooter.getRoot());
 
         CommandBox commandBox = new CommandBox(this::executeCommand);
-        //commandBoxPlaceholder.getChildren().add(commandBox.getRoot());
 
         // Initialize terminal components
         terminalResultDisplay = new ResultDisplay();
