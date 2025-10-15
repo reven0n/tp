58629af91fd6a50077b09ba8c732570a54098c5e
rizrn <89007417+rizrn@users.noreply.github.com>
2025-10-15 17:28:55 +0800

    Add tests for UniqueEventList

--- src/test/java/nusemp/testutil/TypicalEvents.java
+++ src/test/java/nusemp/testutil/TypicalEvents.java
@@ -8,12 +8,12 @@ import nusemp.model.event.EventName;
  * A utility class containing a list of {@code Event} objects to be used in tests.
  */
 public class TypicalEvents {
-    public static final Event MEETING_NO_PARTICIPANTS = new Event(
+    public static final Event MEETING_EMPTY = new Event(
             new EventName("MEETING"), new EventDate("01-10-2025 14:00"));
-    public static final Event MEETING_WITH_PARTICIPANTS = new Event(
+    public static final Event MEETING_FILLED = new Event(
             new EventName("MEETING"), new EventDate("01-10-2025 14:00"), TypicalPersons.getTypicalPersons());
-    public static final Event CONFERENCE_NO_PARTICIPANTS = new Event(
+    public static final Event CONFERENCE_EMPTY = new Event(
             new EventName("CONFERENCE"), new EventDate("29-02-2024 09:00"));
-    public static final Event CONFERENCE_WITH_PARTICIPANTS = new Event(
+    public static final Event CONFERENCE_FILLED = new Event(
             new EventName("CONFERENCE"), new EventDate("29-02-2024 09:00"));
 }
