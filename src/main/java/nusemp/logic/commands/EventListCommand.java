a45b9e97a018ddfcd0ae899c9731e78c2500ba36
CZX <CZX123@users.noreply.github.com>
2025-10-16 03:23:25 +0800

    Standardize success and error messages

--- src/main/java/nusemp/logic/commands/EventListCommand.java
+++ src/main/java/nusemp/logic/commands/EventListCommand.java
@@ -13,6 +13,8 @@ public class EventListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
+    public static final String MESSAGE_SUCCESS = "Successfully listed all %1$s event(s)";
+
     public static final String MESSAGE_USAGE = CommandType.EVENT + " " + COMMAND_WORD
             + ": Lists all events.\n"
             + "Example: " + CommandType.EVENT + " " + COMMAND_WORD;
@@ -22,7 +24,6 @@ public class EventListCommand extends Command {
     public CommandResult execute(Model model) {
         requireNonNull(model);
         model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
-        return new CommandResult(String.format(Messages.MESSAGE_EVENTS_LISTED_OVERVIEW,
-                model.getFilteredEventList().size()));
+        return new CommandResult(String.format(MESSAGE_SUCCESS, model.getFilteredEventList().size()));
     }
 }
