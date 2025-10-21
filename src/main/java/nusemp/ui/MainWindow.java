f7fd7e9d24b40455bccb8bb1eb351de80ec347e8
ywijie <yijieew@gmail.com>
2025-10-21 12:16:57 +0800

    Fix PR issues.

--- src/main/java/nusemp/ui/MainWindow.java
+++ src/main/java/nusemp/ui/MainWindow.java
@@ -1,6 +1,6 @@
 package nusemp.ui;
 
-import java.awt.*;
+import java.awt.Desktop;
 import java.net.URI;
 import java.util.logging.Logger;
 
@@ -105,17 +105,25 @@ public class MainWindow extends UiPart<Stage> {
 
 
         SVGPath svgPath = new SVGPath();
-        svgPath.setContent("M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z");
+        svgPath.setContent("M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 " +
+                "0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z");
         svgPath.setStyle("-fx-fill: lightgray; -fx-stroke: #222; -fx-stroke-width: 1;");
         contactsToggle.setGraphic(svgPath);
 
         SVGPath svgPath1 = new SVGPath();
-        svgPath1.setContent("M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z");
+        svgPath1.setContent("M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1" +
+                " 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0" +
+                " 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.2" +
+                "5h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V" +
+                "15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h." +
+                "008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z");
         svgPath1.setStyle("-fx-fill: lightgray; -fx-stroke: #222; -fx-stroke-width: 1;");
         eventsToggle.setGraphic(svgPath1);
 
         SVGPath svgPath2 = new SVGPath();
-        svgPath2.setContent("M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z");
+        svgPath2.setContent("M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-" +
+                "4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7" +
+                ".5 0Z");
         svgPath2.setStyle("-fx-fill: lightgray; -fx-stroke: lightgray; -fx-stroke-width: 1");
         themeToggle.setGraphic(svgPath2);
 
@@ -334,7 +342,8 @@ public class MainWindow extends UiPart<Stage> {
             getClass().getResource("/view/Extensions.css").toExternalForm()
         );
         SVGPath svgPath = new SVGPath();
-        svgPath.setContent("M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z");
+        svgPath.setContent("M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.7" +
+                "48-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z");
         svgPath.setStyle("-fx-fill: lightgray;");
         themeToggle.setGraphic(svgPath);
         isDarkTheme = false;
@@ -350,7 +359,9 @@ public class MainWindow extends UiPart<Stage> {
             getClass().getResource("/view/Extensions.css").toExternalForm()
         );
         SVGPath svgPath = new SVGPath();
-        svgPath.setContent("M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z");
+        svgPath.setContent("M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773" +
+                "-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 " +
+                "1 7.5 0Z");
         svgPath.setStyle("-fx-fill: lightgray; -fx-stroke: lightgray; -fx-stroke-width: 1");
         themeToggle.setGraphic(svgPath);
         isDarkTheme = true;
