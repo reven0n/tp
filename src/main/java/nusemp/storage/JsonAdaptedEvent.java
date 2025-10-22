6e5db15bd5f0bc2b3a789f78d6154c5f2c9dc98b
rizrn <89007417+rizrn@users.noreply.github.com>
2025-10-23 00:40:03 +0800

    Add Storage for RSVP

--- src/main/java/nusemp/storage/JsonAdaptedEvent.java
+++ src/main/java/nusemp/storage/JsonAdaptedEvent.java
@@ -8,12 +8,15 @@ import java.util.stream.Collectors;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import javafx.util.Pair;
 
 import nusemp.commons.exceptions.IllegalValueException;
 import nusemp.model.ReadOnlyAppData;
 import nusemp.model.contact.Contact;
 import nusemp.model.event.ContactStatus;
 import nusemp.model.event.Event;
+import nusemp.model.event.Status;
+import nusemp.model.event.exceptions.InvalidStatusException;
 import nusemp.model.fields.Address;
 import nusemp.model.fields.Date;
 import nusemp.model.fields.Email;
@@ -27,6 +30,8 @@ class JsonAdaptedEvent {
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Event's %s field is missing!";
     public static final String MISSING_PARTICIPANT_EMAIL_MESSAGE =
             "Participant with email %s not found in contact list";
+    public static final String INVALID_PARTICIPANT_STATUS_MESSAGE =
+            "Participant status is invalid for participant with email %s";
 
     private final String name;
     private final String date;
@@ -34,7 +39,7 @@ class JsonAdaptedEvent {
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private final String address;
 
-    private final List<String> participantEmails = new ArrayList<>();
+    private final List<Pair<String, String>> partcipantStatuses = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedEvent} with the given event details.
@@ -42,12 +47,12 @@ class JsonAdaptedEvent {
     @JsonCreator
     public JsonAdaptedEvent(@JsonProperty("name") String name, @JsonProperty("date") String date,
             @JsonProperty("address") String address,
-            @JsonProperty("participantEmails") List<String> participantEmails) {
+            @JsonProperty("participantStatuses") List<Pair<String, String>> participantStatuses) {
         this.name = name;
         this.date = date;
         this.address = address;
-        if (participantEmails != null) {
-            this.participantEmails.addAll(participantEmails);
+        if (participantStatuses != null) {
+            this.partcipantStatuses.addAll(participantStatuses);
         }
     }
 
@@ -58,9 +63,10 @@ class JsonAdaptedEvent {
         name = source.getName().value;
         date = source.getDate().toString();
         address = source.getAddress().value;
-        participantEmails.addAll(source.getParticipants().stream()
-                .map(status -> status.getContact().getEmail().value)
-                .collect(Collectors.toList()));
+        partcipantStatuses.addAll(source.getParticipants().stream()
+                .map(status -> new Pair<>(
+                        status.getContact().getEmail().value, status.getStatus().toString()))
+                .toList());
     }
 
     /**
@@ -97,13 +103,20 @@ class JsonAdaptedEvent {
         }
 
         final List<ContactStatus> modelParticipants = new ArrayList<>();
-        for (String email : participantEmails) {
+        for (Pair<String, String> contactStatus : partcipantStatuses) {
+            String email = contactStatus.getKey();
+            String statusStr = contactStatus.getValue();
             if (!Email.isValidEmail(email)) {
                 throw new IllegalValueException(Email.MESSAGE_CONSTRAINTS);
             }
             Contact participant = findContactByEmail(appData, email).orElseThrow(() ->
                     new IllegalValueException(String.format(MISSING_PARTICIPANT_EMAIL_MESSAGE, email)));
-            modelParticipants.add(new ContactStatus(participant)); // Not handling status here
+            try {
+                Status status = Status.convertStringToStatus(statusStr);
+                modelParticipants.add(new ContactStatus(participant, status));
+            } catch (InvalidStatusException e) {
+                throw new IllegalValueException(String.format(INVALID_PARTICIPANT_STATUS_MESSAGE, email));
+            }
         }
 
         return new Event(modelName, modelDate, modelAddress, modelParticipants);
