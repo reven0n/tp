7fba0a6fcd822b766e434c8b163f2be073d583ef
ywijie <yijieew@gmail.com>
2025-10-29 02:23:22 +0800

    Update Contacts to correctly reflect RSVP updates for events belonging to contact

--- src/main/java/nusemp/logic/commands/event/EventRsvpCommand.java
+++ src/main/java/nusemp/logic/commands/event/EventRsvpCommand.java
@@ -16,9 +16,11 @@ import nusemp.logic.commands.CommandType;
 import nusemp.logic.commands.exceptions.CommandException;
 import nusemp.model.Model;
 import nusemp.model.contact.Contact;
+import nusemp.model.contact.exceptions.DuplicateContactException;
 import nusemp.model.event.Event;
 import nusemp.model.event.Participant;
 import nusemp.model.event.ParticipantStatus;
+import nusemp.model.event.exceptions.DuplicateEventException;
 import nusemp.model.event.exceptions.ParticipantNotFoundException;
 
 /**
@@ -62,9 +64,21 @@ public class EventRsvpCommand extends Command {
         Event eventToRsvp = getEventFromModel(model);
         Participant updatedParticipant = new Participant(contactToRsvp, status);
 
+
         try {
             Event rsvpedEvent = eventToRsvp.withUpdatedParticipant(updatedParticipant);
             model.setEvent(eventToRsvp, rsvpedEvent);
+            List<Contact> contacts = model.getFilteredContactList();
+            for (Contact contact : contacts) {
+                if (contact.hasEvent(eventToRsvp) || contact.hasEventWithName(eventToRsvp.getName().value)) {
+                    try {
+                        Contact updatedContact = contact.removeEvent(eventToRsvp).addEvent(rsvpedEvent);
+                        model.setContact(contact, updatedContact);
+                    } catch (DuplicateEventException | DuplicateContactException e) {
+                        throw new CommandException("Failed to update contact events after RSVP: " + e.getMessage());
+                    }
+                }
+            }
             return new CommandResult(String.format(MESSAGE_SUCCESS,
                     Messages.format(eventToRsvp), Messages.format(contactToRsvp)));
         } catch (ParticipantNotFoundException e) {
